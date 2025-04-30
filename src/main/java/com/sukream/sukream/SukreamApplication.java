813c9ff4a7f4d7fd696ab338f885f4cb1971c815
Jung <102217402+al1kite@users.noreply.github.com>
2025-04-30 17:02:10 +0900

    init: Sukream API Application 프로젝트 초기화

---
+++ src/main/java/com/sukream/sukream/SukreamApplication.java
@@ -0,0 +1,13 @@
+package com.sukream.sukream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SukreamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SukreamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sukream/sukream/SukreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sukream.sukream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SukreamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
