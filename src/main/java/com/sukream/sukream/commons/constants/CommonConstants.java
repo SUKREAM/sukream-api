9f965d0335d07c9fa8ceb4eebf2571945ea11ab6
Jung <102217402+al1kite@users.noreply.github.com>
2025-05-10 10:24:10 +0900

    feature: CommonConstants 설계 - 자주 사용하는 Constants 정리

---
+++ src/main/java/com/sukream/sukream/commons/constants/CommonConstants.java
@@ -0,0 +1,8 @@
+package com.sukream.sukream.commons.constants;
+
+
+public class CommonConstants {
+    public static final String EMAIL = "email";
+    public static final String NAME = "name";
+
+}
