890bec8d927195ca5fd3af1db7cbd410d7800e3b
Jung <102217402+al1kite@users.noreply.github.com>
2025-05-10 22:29:51 +0900

    feature: AuthDelegate 설정 이메일로 사용자를 찾아 인증 정보를 제공하는 UserDetailsService 구현체 - Spring Security에서 로그인 시 사용자 정보를 조회

---
+++ src/main/java/com/sukream/sukream/domains/auth/service/AuthDelegate.java
@@ -0,0 +1,30 @@
+package com.sukream.sukream.domains.auth.service;
+
+import com.nimbusds.openid.connect.sdk.claims.UserInfo;
+import com.sukream.sukream.domains.auth.repository.UserInfoRepository;
+import com.sukream.sukream.domains.user.domain.dto.UserPrincipal;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class AuthDelegate implements UserDetailsService {
+    private final UserInfoRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findUserByEmail(email)
+                .map(UserPrincipal::from)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found: " + email));
+    }
+
+}
+
