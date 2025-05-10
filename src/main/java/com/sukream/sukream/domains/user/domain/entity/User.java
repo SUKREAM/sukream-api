850a6bb1ba8296f794d476770394e5bdb7af9696
Jung <102217402+al1kite@users.noreply.github.com>
2025-05-10 10:55:45 +0900

    feature: User Entity 설계 User 관련 주요 엔티티 추가 - 사용자 이름 필드 (NotNull) - 사용자 이메일 필드 (NotNull) - 사용자 전화번호 (NotNull) - 사용자 비밀번호 (NotNull) - 사용자 프로필 이미지 - 사용자 계정 활성화 여부 (NotNull : default true) 생성 시 PrePersist 를 통한 true 설정 - 사용자 권한 (roles Set<String> 형태)

---
+++ src/main/java/com/sukream/sukream/domains/user/domain/entity/User.java
@@ -0,0 +1,61 @@
+package com.sukream.sukream.domains.user.domain.entity;
+
+import com.sukream.sukream.commons.jpa.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.*;
+import jakarta.validation.constraints.NotNull;
+import org.hibernate.annotations.Comment;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Getter
+@Entity
+@Builder
+@Comment("사용자 테이블")
+public class User extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Column(name = "name", nullable = false)
+    @Comment("사용자 이름")
+    private String name;
+
+    @NotNull
+    @Column(name = "email", nullable = false)
+    @Comment("사용자 이메일")
+    private String email;
+
+    @NotNull
+    @Column(name = "phone_number", nullable = false)
+    @Comment("사용자 전화번호")
+    private String phoneNumber;
+
+    @NotNull
+    @Column(name = "password", nullable = false)
+    @Comment("사용자 비밀번호")
+    private String password;
+
+    @Column(name = "profile_image", nullable = true)
+    @Comment("사용자 프로필 이미지")
+    private String profileImage;
+
+    @NotNull
+    @Column(name = "is_active", nullable = false)
+    @Comment("사용자 계정 활성화 여부")
+    private boolean isActive;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"))
+    @Column(name = "role")
+    private Set<String> roles = new HashSet<>();
+
+    @PrePersist
+    protected void onCreate() {
+        this.isActive = true;
+    }
+}
