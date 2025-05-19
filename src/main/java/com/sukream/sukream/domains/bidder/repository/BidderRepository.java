d95168d102af0e3a03ed45128fb3f44505b49713
jiwoo <1114jiwoo@gmail.com>
2025-05-19 20:01:33 +0900

    feat: 상품 ID 기준 입찰자 목록 조회 메서드 추가

---
+++ src/main/java/com/sukream/sukream/domains/bidder/repository/BidderRepository.java
@@ -0,0 +1,12 @@
+package com.sukream.sukream.domains.bidder.repository;
+
+import com.sukream.sukream.domains.bidder.entity.Bidder;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface BidderRepository extends JpaRepository<Bidder, Long> {
+    List<Bidder> findByProduct_ProductIdOrderByPriceDesc(Long productId);
+}
