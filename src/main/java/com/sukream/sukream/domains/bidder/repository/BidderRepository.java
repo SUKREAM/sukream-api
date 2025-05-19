e1dd21b6e5bca4db93219df30ac2ea0ac716727f
jiwoo <1114jiwoo@gmail.com>
2025-05-19 20:57:10 +0900

    feat: User 기준 추가

--- src/main/java/com/sukream/sukream/domains/bidder/repository/BidderRepository.java
+++ src/main/java/com/sukream/sukream/domains/bidder/repository/BidderRepository.java
@@ -1,6 +1,7 @@
 package com.sukream.sukream.domains.bidder.repository;
 
 import com.sukream.sukream.domains.bidder.entity.Bidder;
+import com.sukream.sukream.domains.user.domain.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +10,5 @@ import java.util.List;
 @Repository
 public interface BidderRepository extends JpaRepository<Bidder, Long> {
     List<Bidder> findByProduct_ProductIdOrderByPriceDesc(Long productId);
+    List<Bidder> findAllByUser(User user);
 }
