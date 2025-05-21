d2678b35ab8e0e7a1a0d9ca35fdc40e343574630
jiwoo <1114jiwoo@gmail.com>
2025-05-21 19:08:58 +0900

    fix: productStatus enum 사용

--- src/main/java/com/sukream/sukream/domains/mypage/domain/dto/SalesResponseDto.java
+++ src/main/java/com/sukream/sukream/domains/mypage/domain/dto/SalesResponseDto.java
@@ -1,5 +1,7 @@
 package com.sukream.sukream.domains.mypage.domain.dto;
 
+import com.sukream.sukream.domains.product.entity.ProductStatus;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -7,10 +9,11 @@ import java.time.LocalDateTime;
 
 @Getter
 @Builder
+@AllArgsConstructor
 public class SalesResponseDto {
     private Long productId;
     private String productName;
     private String productImage;
     private LocalDateTime createdAt; //판매시간
-    private String status; //판매중 / 판매완료
+    private ProductStatus status;
 }
