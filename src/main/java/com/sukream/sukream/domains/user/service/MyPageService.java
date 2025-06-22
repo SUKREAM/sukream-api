6377ee931889bf45823c3b374a73a6c544809c5e
Jung <102217402+al1kite@users.noreply.github.com>
2025-06-22 09:14:03 +0900

    fix: 회원정보 수정 로직 수정

--- src/main/java/com/sukream/sukream/domains/user/service/MyPageService.java
+++ src/main/java/com/sukream/sukream/domains/user/service/MyPageService.java
@@ -2,23 +2,32 @@ package com.sukream.sukream.domains.user.service;
 
 import com.sukream.sukream.domains.auth.repository.UserInfoRepository;
 import com.sukream.sukream.domains.auth.security.LoginManager;
+import com.sukream.sukream.domains.auth.service.AuthDelegate;
 import com.sukream.sukream.domains.user.domain.entity.Users;
 import com.sukream.sukream.domains.user.domain.request.UserRequest;
 import com.sukream.sukream.domains.user.domain.response.UserResponse;
 import com.sukream.sukream.domains.user.mapper.UserRequestMapper;
 import com.sukream.sukream.domains.user.mapper.UserResponseMapper;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
 public class MyPageService {
 
-    private final UserRequestMapper userRequestMapper;
+    private final UserInfoRepository userInfoRepository;
     private final UserResponseMapper userResponseMapper;
+    private final AuthDelegate authDelegate;
 
+    @Transactional
     public UserResponse myPage(UserRequest userRequest){
-        Users userInfo = LoginManager.getUserDetails().getUser();
+        Users userInfo = userInfoRepository.findById(Objects.requireNonNull(LoginManager.getUserDetails()).getUser().getId())
+                .orElseThrow(() -> new RuntimeException("User not found"));
+        userRequest.setPassword((authDelegate.passwordEncoding(userRequest.getPassword())));
+
         userInfo.updateUserInfo(userRequest);
 
         return userResponseMapper.toDto(userInfo);
